a16f92712b2de8b1192d4e799e1fde411e24ee2a
Garrett Honeycutt <code@garretthoneycutt.com>
2014-01-10 14:29:50 -0500

    WIP

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,19 @@
 # Module to manage types
 #
 class types (
-  $mounts = undef,
+  $mounts          = undef,
+  $use_hiera_array = true,
 ) {
 
   if $mounts != undef {
     validate_hash($mounts)
-    create_resources('types::mount',$mounts)
+
+    if $use_hiera_array == true {
+      $mounts_real = hiera_array($mounts)
+    } else {
+      $mounts_real = $mounts
+    }
+
+    create_resources('types::mount',$mounts_real)
   }
 }
