562f735a34b07e30a33c081c876b2b98f3fa10bc
Johan Wennerberg <j.wennerberg@gmail.com>
2014-02-03 00:01:54 +0100

    Add support for type 'cron'

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,14 @@
 # Module to manage types
 #
 class types (
+  $cron   = undef,
   $mounts = undef,
 ) {
+
+  if $cron != undef {
+    validate_hash($cron)
+    create_resources('types::cron',$cron)
+  }
 
   if $mounts != undef {
     validate_hash($mounts)
