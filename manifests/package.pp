3ea17e8c36cc9752608752bd5c23f3796482b399
eabecho <abel.chow@ericsson.com>
2015-01-26 17:33:10 -0800

    Clean up non-working paramters

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,11 +3,9 @@
 define types::package (
   $ensure            = 'present',
   $adminfile         = undef,
-  $allow_virtual     = undef,
   $allowcdrom        = undef,
   $configfiles       = undef,
   $install_options   = undef,
-#  $package_settings  = undef,
   $provider          = undef,
   $responsefile      = undef,
   $source            = undef,
@@ -20,18 +18,10 @@ define types::package (
   if $adminfile != undef {
     validate_absolute_path($adminfile)
   }
-#  if $allow_virtual != undef {
-#    validate_re($allow_virtual, '^(true)|(false)|(yes)|(no)$',
-#      "types::package::${name}::allow_virtual is invalid and does not match the regex.")
-#  }
-#  if $allowcdrom != undef {
-#    validate_re($allowcdrom, '^(true)|(false)$',
-#      "types::package::${name}::allowcdrom is invalid and does not match the regex.")
-#  }
-#  if $configfiles != undef {
-#    validate_re($configfiles, '^(keep)|(replace)$',
-#      "types::package::${name}::configfiles is invalid and does not match the regex.")
-#  }
+  if $configfiles != undef {
+    validate_re($configfiles, '^(keep)|(replace)$',
+      "types::package::${name}::configfiles is invalid and does not match the regex.")
+  }
   if $responsefile != undef {
     validate_absolute_path($responsefile)
   }
@@ -39,11 +29,8 @@ define types::package (
   package { $name:
     ensure            => $ensure,
     adminfile         => $adminfile,
-#    allow_virtual     => $allow_virtual,
-    allowcdrom        => $allowcdrom,
     configfiles       => $configfiles,
     install_options   => $install_options,
-#    package_settings  => $package_settings,
     provider          => $provider,
     responsefile      => $responsefile,
     source            => $source,
