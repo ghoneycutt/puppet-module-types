2ccdb7f504f0842b500d19417e0660bd014baed0
Phil Friderici <phil.friderici@elits.com>
2023-04-11 08:53:59 +0000

    Remove outdated validations for booleans

--- manifests/init.pp
+++ manifests/init.pp
@@ -78,59 +78,8 @@ class types (
   Boolean $selbooleans_hiera_merge = true,
   Boolean $services_hiera_merge    = true,
 ) {
-  if is_string($crons_hiera_merge) {
-    $crons_hiera_merge_real = str2bool($crons_hiera_merge)
-  } else {
-    $crons_hiera_merge_real = $crons_hiera_merge
-  }
-  validate_bool($crons_hiera_merge_real)
-
-  $execs_hiera_merge_bool = str2bool($execs_hiera_merge)
-
-  if is_string($file_lines_hiera_merge) {
-    $file_lines_hiera_merge_real = str2bool($file_lines_hiera_merge)
-  } else {
-    $file_lines_hiera_merge_real = $file_lines_hiera_merge
-  }
-  validate_bool($file_lines_hiera_merge_real)
-
-  if is_string($files_hiera_merge) {
-    $files_hiera_merge_real = str2bool($files_hiera_merge)
-  } else {
-    $files_hiera_merge_real = $files_hiera_merge
-  }
-  validate_bool($files_hiera_merge_real)
-
-  if is_string($mounts_hiera_merge) {
-    $mounts_hiera_merge_real = str2bool($mounts_hiera_merge)
-  } else {
-    $mounts_hiera_merge_real = $mounts_hiera_merge
-  }
-  validate_bool($mounts_hiera_merge_real)
-
-  if is_string($packages_hiera_merge) {
-    $packages_hiera_merge_real = str2bool($packages_hiera_merge)
-  } else {
-    $packages_hiera_merge_real = $packages_hiera_merge
-  }
-  validate_bool($packages_hiera_merge_real)
-
-  if is_string($selbooleans_hiera_merge) {
-    $selbooleans_hiera_merge_real = str2bool($selbooleans_hiera_merge)
-  } else {
-    $selbooleans_hiera_merge_real = $selbooleans_hiera_merge
-  }
-  validate_bool($selbooleans_hiera_merge_real)
-
-  if is_string($services_hiera_merge) {
-    $services_hiera_merge_real = str2bool($services_hiera_merge)
-  } else {
-    $services_hiera_merge_real = $services_hiera_merge
-  }
-  validate_bool($services_hiera_merge_real)
-
   if $crons != {} {
-    if $crons_hiera_merge_real == true {
+    if $crons_hiera_merge == true {
       $crons_real = hiera_hash('types::crons')
     } else {
       $crons_real = $crons
@@ -140,7 +89,7 @@ class types (
   }
 
   if $execs != {} {
-    if $execs_hiera_merge_bool == true {
+    if $execs_hiera_merge == true {
       $execs_real = hiera_hash('types::execs')
     } else {
       $execs_real = $execs
@@ -150,7 +99,7 @@ class types (
   }
 
   if $file_lines != {} {
-    if $file_lines_hiera_merge_real == true {
+    if $file_lines_hiera_merge == true {
       $file_lines_real = hiera_hash('types::file_lines')
     } else {
       $file_lines_real = $file_lines
@@ -160,7 +109,7 @@ class types (
   }
 
   if $files != {} {
-    if $files_hiera_merge_real == true {
+    if $files_hiera_merge == true {
       $files_real = hiera_hash('types::files')
     } else {
       $files_real = $files
@@ -170,7 +119,7 @@ class types (
   }
 
   if $mounts != {} {
-    if $mounts_hiera_merge_real == true {
+    if $mounts_hiera_merge == true {
       $mounts_real = hiera_hash('types::mounts')
     } else {
       $mounts_real = $mounts
@@ -180,7 +129,7 @@ class types (
   }
 
   if $packages != {} {
-    if $packages_hiera_merge_real == true {
+    if $packages_hiera_merge == true {
       $packages_real = hiera_hash('types::packages')
     } else {
       $packages_real = $packages
@@ -190,7 +139,7 @@ class types (
   }
 
   if $selbooleans != {} {
-    if $selbooleans_hiera_merge_real == true {
+    if $selbooleans_hiera_merge == true {
       $selbooleans_real = hiera_hash('types::selboolean')
     } else {
       $selbooleans_real = $selbooleans
@@ -200,7 +149,7 @@ class types (
   }
 
   if $services != {} {
-    if $services_hiera_merge_real == true {
+    if $services_hiera_merge == true {
       $services_real = hiera_hash('types::services')
     } else {
       $services_real = $services
